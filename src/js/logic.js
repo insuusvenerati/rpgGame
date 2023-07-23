6d6241edc7248e2316608cf6e118c78704ba69f0
modjordan <mod.jordan@icloud.com>
2023-07-23 10:41:08 -0700

    First test pass.

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,3 +1,15 @@
 export class Character {
-  constructor() { }
+  constructor(name, type, str, int, char) {
+    this.name = name;
+    this.type = type;
+    this.str = str;
+    this.int = int;
+    this.char = char;
+  }
+
+  name;
+  type;
+  str = 0;
+  int = 0;
+  char = 0;
 }
