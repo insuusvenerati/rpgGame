14f160a255526fd066d1f97dfc68893451233dd0
modjordan <mod.jordan@icloud.com>
2023-07-23 12:01:29 -0700

    First bad fail for character inventory.

--- __tests__/gameLogic.tests.js
+++ __tests__/gameLogic.tests.js
@@ -2,21 +2,26 @@ import { Character } from "../src/js/logic";
 
 describe("Character", () => {
 
-  it("should should create a character with a name, class and stats", () => {
+  it("should should create a character with a name and class", () => {
     const char = new Character("Bob", "Warrior");
     expect(char.name).toBe("Bob")
     expect(char.type).toBe("Warrior")
-    expect(char.str).toBe(1)
-    expect(char.int).toBe(1)
-    expect(char.char).toBe(1)
+  })
+
+  it("should create a character with inital xp", () => {
+    const char = new Character("Bob", "Warrior");
     expect(char.xp).toBe(100)
-    expect(char.level).toBe(1)
   })
 
   it("should add to xp", () => {
     const newChar = new Character("Bob", "Warrior");
     newChar.addXP(100);
     expect(newChar.xp).toBe(200)
+  })
+
+  it("should create a character with inital level as 1", () => {
+    const newChar = new Character("Bob", "Warrior");
+    expect(newChar.level).toBe(1)
   })
 
 
@@ -26,6 +31,13 @@ describe("Character", () => {
     expect(char.level).toBe(4)
   })
 
+  it("should create a character with initial stats", () => {
+    const char = new Character("Bob", "Warrior");
+    expect(char.str).toBe(1)
+    expect(char.int).toBe(1)
+    expect(char.char).toBe(1)
+  })
+
   it("should increase stats for every level", () => {
     const char = new Character("Bob", "Warrior")
     char.addXP(200)
@@ -33,5 +45,16 @@ describe("Character", () => {
     expect(char.int).toBe(3)
     expect(char.char).toBe(3)
   })
+
+  it("should create a character with intially empty inventory", () => {
+    const char = new Character("Bob", "Warrior")
+    expect(char.inventory).toEqual([])
+  })
+
 })
+
+
+
+
+// Items as a new class, Inventory as a property in Character class;
 
